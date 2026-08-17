57fc26f38fb298aa830b5b07107733da72dea98d
shiro234234 <y.ashika.debuhamu@gmail.com>
2026-08-17 12:30:32 +0900

    Add files via upload

--- メニュー選択アプリ.py
+++ メニュー選択アプリ.py
@@ -35,7 +35,7 @@ def analyze_images(_images, prompt_text):
     contents = [*_images, prompt_text]
     
     response = client.models.generate_content(
-        model='gemini-2.5-flash',
+        model='gemini-flash',
         contents=contents,
         config=types.GenerateContentConfig(
             response_mime_type="application/json",
